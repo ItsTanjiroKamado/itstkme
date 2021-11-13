b7f7620af43a6e02c7416958586f5082746b1001
ItsTanjiroKamado <85858892+ItsTanjiroKamado@users.noreply.github.com>
2021-11-13 22:31:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,16 +11,14 @@ app.listen(port, () => {
     log(`itstk.me at http://localhost:${port}`)
   })
 
-  const morganMiddleware = morgan(function (tokens, req, res) {
-    return [
-        '\n\n\n',
-        chalk.hex('#34ace0').bold(tokens.method(req, res)),
-        chalk.hex('#ffb142').bold(tokens.status(req, res)),
-        chalk.hex('#ff5252').bold(tokens.url(req, res)),
-        chalk.hex('#2ed573').bold(tokens['response-time'](req, res) + ' ms'),
-        chalk.hex('#f78fb3').bold('@ ' + tokens.date(req, res)),
-        '\n\n\n',
+const morganMiddleware = morgan(function(tokens, req, res) {
+    if (!blocked.includes(req.headers['x-forwarded-for'])) return [
+      chalk.hex('#34ace0').bold(tokens.method(req, res)),
+      chalk.hex('#ffb142').bold(tokens.status(req, res) || 200),
+      chalk.hex('#ff5252').bold(tokens.url(req, res)),
+      chalk.hex('#2ed573').bold((tokens['response-time'](req, res) || 0) + ' ms'),
+      chalk.hex('#f78fb3').bold('@ ' + tokens.date(req, res)),
     ].join(' ');
-});
+  });
 
 app.use(morganMiddleware);
