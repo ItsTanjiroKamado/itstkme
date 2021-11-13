8d1a95c0e5c01ef4ce6df4e31ff0c9914565fdf2
ItsTanjiroKamado <85858892+ItsTanjiroKamado@users.noreply.github.com>
2021-11-13 22:58:10 +0530

    why tf am i doing this kekw

--- index.js
+++ index.js
@@ -4,13 +4,11 @@ const log = console.log;
 const port = process.env.PORT;
 
 app.use((req, res, next) => {
-    log(`${req.method} - ${req.path} - ${req.ip}`);
+    log(`${req.method} - ${req.path}`);
     next();
 })
 app.use(e.static(__dirname + '/static'))
 
 app.listen(port, () => {
-    log(`itstk.me at http://localhost:${port}`)
+    log(`Listening to PORT 3000 \n Powered by https://railway.app/ \n Hosting: \n https://itstkme-production.up.railway.app/ \n https://itstk.me \n The web is up and working af! \n Source code at https://itstk.me/github/web `)
   })
-
-  
