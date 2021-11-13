62cc1fa37d6d79b847e63da2ea5f91447fa6d50a
ItsTanjiroKamado <85858892+ItsTanjiroKamado@users.noreply.github.com>
2021-11-13 21:40:39 +0530

    doing shit

--- index.js
+++ index.js
@@ -4,11 +4,13 @@ const log = console.log;
 const port = process.env.PORT;
 
 app.use((req, res, next) => {
-    log(`GET ${req.method} - ${req.path}`);
+    log(`Request ${req.method} - ${req.path}`);
     next();
 })
-app.use(e.static(__dirname + '/public_html'))
+app.use(e.static(__dirname + '/static'))
 
 app.listen(port, () => {
     log(`itstk.me at http://localhost:${port}`)
   })
+
+  
